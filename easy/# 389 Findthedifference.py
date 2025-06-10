6e7ea140f479337f945f985b7e5ea3e5a658cca8
flugeldan <channelsultan@gmail.com>
2025-06-10 15:11:59 +0500

    Add files via upload

---
+++ easy/# 389 Findthedifference.py
@@ -0,0 +1,13 @@
+# 389. Find the Difference
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/find-the-difference/description/?envType=study-plan-v2&envId=programming-skills
+
+class Solution(object):
+    def findTheDifference(self, s, t):
+        for i in t:
+            if s.count(i) == t.count(i):
+                continue
+            elif s.count(i) != t.count(i):
+                return (i)
+
+        
